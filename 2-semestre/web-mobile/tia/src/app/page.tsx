66c94403cb9e0a0bcec729f95e8d9fa7ab44ecfa
Luis Felipe <luisfelipesdn12@gmail.com>
2024-03-18 11:23:02 -0300

    Add sound

--- 2-semestre/web-mobile/tia/src/app/page.tsx
+++ 2-semestre/web-mobile/tia/src/app/page.tsx
@@ -1,11 +1,20 @@
-import VoteCard from "@/components/vote-card";
+"use client";
+
+import TiaCard from "@/components/tia-card";
+
 
 export default function Home() {
+  const play = () => {
+    var audio: HTMLAudioElement = document.getElementById("igor-audio") as HTMLAudioElement;
+    audio.play();
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center align-middle p-24 gap-4">
+    <main onClick={() => play()} className="flex min-h-screen flex-col items-center justify-center align-middle p-24 gap-4">
+      <audio id="igor-audio" src="/igor.mp3"></audio>
       <ul className="flex gap-4 flex-wrap justify-center items-center">
         {Array.from(Array(1).keys()).map(n => (
-          <VoteCard key={n} />
+          <TiaCard key={n} />
         ))}
       </ul>
     </main>
---
+++ 2-semestre/web-mobile/tia/src/components/tia-card.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+    Card,
+    CardContent,
+    CardHeader
+} from "@/components/ui/card";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import ReactCardFlip from "react-card-flip";
+import { Input } from "./ui/input";
+
+export default function TiaCard() {
+    const [selected, setSelected] = useState<boolean>();
+
+    useEffect(() => {
+        const id = setInterval(() => setSelected(s => !s), 4000);
+        return () => clearInterval(id);
+    }, []);
+
+    return (
+        <ReactCardFlip
+            infinite={true}
+            flipSpeedBackToFront={1}
+            flipSpeedFrontToBack={1}
+            isFlipped={selected} flipDirection="horizontal">
+            <Card onClick={() => setSelected(true)} className="mack-card shadow-2xl bg-[#A03034] w-[40rem] h-[25rem] flex flex-col justify-center items-center text-center">
+                <CardHeader className="flex justify-center items-center py-2">
+                    <Image alt="Mackenzie" src={"/logo_mack.png"} width={400} height={200} />
+                </CardHeader>
+                <CardContent className="flex">
+                    <Avatar className="w-[9rem] h-[12rem] rounded-none">
+                        <AvatarImage src="/icon.jpg" />
+                        <AvatarFallback className="w-[9rem] h-[12rem] rounded-none">Imagem aqui</AvatarFallback>
+                    </Avatar>
+                    <aside className="w-80 h-[12rem] flex flex-col justify-between px-2">
+                        <Input readOnly className="text-center font-semibold text-xl h-12" type="text" value={"Tyler Okoma"} />
+                        <div className="flex gap-2">
+                            <Input readOnly className="text-center font-semibold text-xl h-12" type="text" value={"0000000-0"} />
+                            <Input readOnly className="text-center font-semibold text-xl h-12" type="text" value={"12/2023"} />
+                        </div>
+                        <Input readOnly className="text-center font-semibold text-xl h-12" type="text" value={"Sistemas de Informação"} />
+                    </aside>
+                </CardContent>
+            </Card>
+            <Card onClick={() => setSelected(false)} className="mack-card shadow-2xl bg-[#A03034] w-[40rem] h-[25rem] flex flex-col justify-center items-center text-center">
+            </Card>
+        </ReactCardFlip>
+    );
+}
